8a761bfa19d54984342eda15d286ff52026f3c19
Tom Fyfe <tomfyfe85@gmail.com>
2023-12-05 12:21:11 +0000

    hello_web_project set up

---
+++ Phase2_Flask_Challenges/hello_web_project/app.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, request
+
+# Create a new Flask app
+app = Flask(__name__)
+
+# == Your Routes Here ==
+
+# == Example Code Below ==
+
+# GET /emoji
+# Returns a emojiy face
+# Try it:
+#   ; curl http://127.0.0.1:5001/emoji
+@app.route('/emoji', methods=['GET'])
+def get_emoji():
+    return ":)"
+
+# This imports some more example routes for you to see how they work
+# You can delete these lines if you don't need them.
+from example_routes import apply_example_routes
+apply_example_routes(app)
+
+# == End Example Code ==
+
+# These lines start the server if you run this file directly
+# They also start the server configured to use the test database
+# if started in test mode.
+if __name__ == '__main__':
+    app.run(debug=True, port=int(os.environ.get('PORT', 5001)))
+
